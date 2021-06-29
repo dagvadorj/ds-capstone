d011278488004c540d70053f994d48215be3eba3
Dagvadorj Galbadrakh <galbadrakh@itu.edu.tr>
2021-06-30 00:58:45 +0800

    Executive summary initial draft was added Code was added to prepare initial data without downloading and unzipping was added in order to save time Regularization code added

--- MovieLens/MovieLens2.R
+++ MovieLens/MovieLens2.R
@@ -1,3 +1,9 @@
+# Executive summary
+# Movie Lens database is examined and models were reviewed and fit for the Hardvard edX Data Science: Capstone project.
+# The initial section of the code is based on the boilerplate code provided at the "Create Train and Final Hold-out Test Sets" section of the course found at https://learning.edx.org/course/course-v1:HarvardX+PH125.9x+1T2021/block-v1:HarvardX+PH125.9x+1T2021+type@sequential+block@e8800e37aa444297a3a2f35bf84ce452/block-v1:HarvardX+PH125.9x+1T2021+type@vertical+block@e9abcdd945b1416098a15fc95807b5db. Following columns were added to the movielens data frame in order to be used as potential predictors: releaseYear, ratingAge, year, month, week, weekday, hour, avgRating, firstGenre.
+# Different models were used by utilizing train method from the caret library. However, every try took unfeasible amount of time in my computer with 16 gig memory. Therefore linear model that examines accumulative biases of predictors were used.
+
+
 ##########################################################
 # Create edx set, validation set (final hold-out test set)
 ##########################################################
@@ -23,8 +29,9 @@ download.file("http://files.grouplens.org/datasets/movielens/ml-10m.zip", dl)
 ratings <- fread(text = gsub("::", "\t", readLines(unzip(dl, "ml-10M100K/ratings.dat"))), col.names = c("userId", "movieId", "rating", "timestamp"))
 movies <- str_split_fixed(readLines(unzip(dl, "ml-10M100K/movies.dat")), "\\::", 3)
 
-ratings <- fread(text = gsub("::", "\t", readLines("ml-10M100K/ratings.dat")), col.names = c("userId", "movieId", "rating", "timestamp"))
-movies <- str_split_fixed(readLines("ml-10M100K/movies.dat"), "\\::", 3)
+# In order to save time, this needs to be run once the dataset zip is donwloaded already
+# ratings <- fread(text = gsub("::", "\t", readLines("ml-10M100K/ratings.dat")), col.names = c("userId", "movieId", "rating", "timestamp"))
+# movies <- str_split_fixed(readLines("ml-10M100K/movies.dat"), "\\::", 3)
 
 colnames(movies) <- c("movieId", "title", "genres")
 
@@ -40,9 +47,7 @@ movies <- as.data.frame(movies) %>% mutate(movieId = as.numeric(movieId),
 
 movielens <- left_join(ratings, movies, by = "movieId")
 
-str_extract(movielens$title, regex("\\((\\d{4})"))
-
-movielens <- movielens %>% mutate(releaseYear = as.integer(str_extract(movielens$title, regex("(\\d{4}$)"))))
+movielens <- movielens %>% mutate(releaseYear = str_extract(movielens$title, "\\((\\d{4})\\)$")) %>% mutate(releaseYear = as.integer(substring(releaseYear, 2, nchar(releaseYear)-1)))
 
 movielens <- movielens %>% mutate(datetime = lubridate::as_datetime(timestamp))
 
@@ -71,15 +76,15 @@ edx <- rbind(edx, removed)
 
 rm(dl, ratings, movies, test_index, temp, movielens, removed)
 
-## Define predictors
-
-
+## TODO Exploratory analysis
 
 summary(edx)
 
-## Explore correlation between predictors
+##
 
-# edx %>% ggplot(aes(hour, rating)) + geom_tile()
+RMSE <- function(predicted, actual) {
+  sqrt(mean((predicted - actual)^2, na.rm = TRUE))
+}
 
 ## Divide into train and test sets
 
@@ -90,32 +95,11 @@ test_index <- createDataPartition(edx$rating, times = 1, p = 0.2, list = FALSE)
 train_set <- edx[-test_index,]
 test_set <- edx[test_index,]
 
+test_set <- test_set %>%semi_join(train_set, by = "movieId") %>% semi_join(train_set, by = "userId")
+
 # TODO Why is 100000 good
 
-train_set_1 <- sample_n(train_set, 100000)
-test_set_1 <- sample_n(test_set, 100000)
-
-test_set_1 <- test_set_1 %>%semi_join(train_set_1, by = "movieId") %>% semi_join(train_set_1, by = "userId")
-
-# plot(train_set_1$weekday, train_set_1$rating)
-
-RMSE <- function(predicted, actual) {
-  sqrt(mean((predicted - actual)^2))
-}
-
-rm(bias_weeks)
-
 memory.limit(9999999999)
-
-y <- train_set_1 %>% 
-  select(userId, movieId, rating) %>%
-  pivot_wider(names_from = "movieId", values_from = "rating") %>%
-  as.matrix()
-gc()
-y[is.na(y)] <- 0
-gc()
-pca <- prcomp(y)
-gc()
 
 ## Effects
 
@@ -126,11 +110,12 @@ gc()
 ## Matrix factorization
 
 ## Choose best predictors
-# TODO Why these predictors
 
 ## Train models
 
 ## Test models
+
+## The logic
 
 mu <- mean(train_set$rating)
 
@@ -138,54 +123,80 @@ bias_movies <- train_set %>%
   group_by(movieId) %>%
   summarize(b_movie = mean(rating - mu))
 
-bias_movies
-
-predicted_ratings <- mu + test_set %>%
+predicted_ratings <- test_set %>%
   left_join(bias_movies, by = "movieId") %>%
-  # mutate(pred = mu + b_movie) %>%
-  pull(b_movie)
+  mutate(pred = mu + b_movie)
 
-predicted_ratings
-
-predicted_ratings
-
-test_set$rating
-
-RMSE(predicted_ratings, test_set$rating)
+RMSE(predicted_ratings$pred, test_set$rating)
 
 bias_users <- train_set %>% 
   left_join(bias_movies, by='movieId') %>%
   group_by(userId) %>%
   summarize(b_user = mean(rating - mu - b_movie))
 
-bias_users
-
 predicted_ratings <- test_set %>% 
   left_join(bias_movies, by='movieId') %>%
   left_join(bias_users, by='userId') %>%
-  mutate(pred = mu + b_movie + b_user) %>%
-  pull(pred)
-RMSE(predicted_ratings, test_set$rating)
+  mutate(pred = mu + b_movie + b_user)
+
+RMSE(predicted_ratings$pred, test_set$rating)
 
 bias_weeks <- train_set %>% 
   left_join(bias_movies, by='movieId') %>%
   left_join(bias_users, by='userId') %>%
-  group_by(week) %>%
+  group_by(avgRating) %>%
   summarize(b_week = mean(rating - mu - b_movie - b_user))
 
-bias_weeks[is.na(bias_weeks)] <- 0
-
-predicted_ratings <- test_set %>% 
+predicted_ratings <- test_set %>%
   left_join(bias_movies, by='movieId') %>%
   left_join(bias_users, by='userId') %>%
-  left_join(bias_weeks, by='week') %>%
-  mutate(pred = mu + b_movie + b_user + b_week) %>%
-  pull(pred)
+  left_join(bias_weeks, by='avgRating') %>%
+  mutate(pred = mu + b_movie + b_user + b_week)
 
-head(predicted_ratings)
+RMSE(predicted_ratings$pred, test_set$rating)
 
-RMSE(predicted_ratings, test_set$rating)
+## Cross validation regularization
 
-## Cross validate models
+lambdas = seq(0, 10, .1)
+lambdas
+
+regularize <- function(train, test, lambda) {
+
+  mu <- mean(train$rating)
+
+  bias_movies <- train %>%
+    group_by(movieId) %>%
+    summarize(b_movie = sum(rating - mu)/(lambda + n()))
+
+  bias_users <- train %>% 
+    left_join(bias_movies, by='movieId') %>%
+    group_by(userId) %>%
+    summarize(b_user = sum(rating - mu - b_movie)/(lambda + n()))
+
+  bias_weeks <- train %>% 
+    left_join(bias_movies, by='movieId') %>%
+    left_join(bias_users, by='userId') %>%
+    group_by(firstGenre) %>%
+    summarize(b_week = sum(rating - mu - b_movie - b_user)/(lambda + n()))
+  
+  predicted_ratings <- test %>%
+    left_join(bias_movies, by='movieId') %>%
+    left_join(bias_users, by='userId') %>%
+    left_join(bias_weeks, by='firstGenre') %>%
+    mutate(pred = mu + b_movie + b_user + b_week)
+
+  RMSE(predicted_ratings$pred, test$rating)
+
+}
+
+dplyr.summarise.inform <- FALSE
+
+results <- sapply(lambdas, function(lambda) {
+  regularize(edx, validation, lambda)
+})
+
+qplot(lambdas, results)
+
+min(results)
 
 ## Validate models
